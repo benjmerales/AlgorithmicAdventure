76bd81354edb13b1519bd507ecd9bc0ca9cfb801
benjmerales <61940743+benjmerales@users.noreply.github.com>
2021-06-02 22:18:29 +0800

    Delete Activities/src directory

--- Activities/src/ConstantFrames.java
+++
@@ -1,6 +0,0 @@
-public interface ConstantFrames{
-    CategoryFrame CF_inter = new CategoryFrame();
-    LevelFrame LF_inter = new LevelFrame();
-    Score S_inter = new Score();
-    Mechanism M_inter = new Mechanism();
-}
